4b1bd650070266c27c7d75e08cedf2967c5f35ab
haungyue03 <huangyue03@baidu.com>
2017-04-25 16:25:15 +0800

    add push script

---
+++ gitpush.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "need branch............."
+    exit 1;
+fi
+
+# 获取执行文件的路径
+workspace=$(cd "$(dirname "$0")"; pwd)
+echo "workspace is "$workspace
+
+cd $workspace
+    git push origin $1:refs/for/$1
+cd -
+echo "done.............."%
